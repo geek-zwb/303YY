121b2bea180d6beb242435119d49a20f8824cadc
Zhong Weibin <weibin.zhong@maxent-inc.com>
2017-08-17 16:47:14 +0800

    take photos

--- pages/photograph/photograph.js
+++ pages/photograph/photograph.js
@@ -2,6 +2,7 @@
  * Created by geekzwb on 2017/8/15.
  * What for:  take pictures
  * pages in the middle of toolbar
+ * bug: 选中图片后 placeHolder 文字没跟随？
  */
 // 获取应用实例
 const app = getApp()
@@ -10,5 +11,38 @@ const app = getApp()
  * 上传照片 page
  */
 Page({
-
+  data: {
+    imgPath: ''
+  },
+  onReady: function () {
+    this.chooseImg()
+  },
+  chooseImg: function () {
+    let that = this
+    wx.chooseImage({
+      success: function(res) {
+        let tempFilePaths = res.tempFilePaths
+        that.setData({
+          imgPath: tempFilePaths
+        })
+        /*wx.saveFile({
+          tempFilePath: tempFilePaths[0],
+          success: function(res) {
+            let savedFilePath = res.savedFilePath
+            that.setData({
+              imgPath: savedFilePath
+            })
+            console.log('savedFilePath', savedFilePath);
+          }
+        })*/
+      }
+    })
+  },
+  /**
+   * 提交照片描述
+   * @param e
+   */
+  bindFormSubmit: function(e) {
+    console.log(e.detail.value.textarea)
+  }
 })
